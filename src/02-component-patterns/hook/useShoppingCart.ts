de359eed174619b3bb485df8967b6f8ecf8234a7
Francisco Garcia Lopez <fran@MacBook-Pro-de-Francisco.local>
2022-01-09 11:25:23 +0100

    Control Props Alt

--- src/02-component-patterns/hook/useShoppingCart.ts
+++ src/02-component-patterns/hook/useShoppingCart.ts
@@ -8,27 +8,15 @@ export const useShoppingCart = () => {
     const onProductCartChange = ({count, product} : {count: number, product: Product}) => {
         setShoppingCart( oldShoppingCart => {
 
-            const productInCart : ProductInCart = oldShoppingCart[product.id] || {...product, count: 0};
-
-            if (Math.max(productInCart.count + count, 0) > 0){
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart, [product.id] : productInCart
-                }
-            }
-
-            const {[product.id]: toDelete, ...rest} = oldShoppingCart;
+            if (count===0) {
+                const {[product.id]: toDelete, ...rest} = oldShoppingCart;
+                console.log({toDelete});
                 return rest
-
-            // if (count===0) {
-            //     const {[product.id]: toDelete, ...rest} = oldShoppingCart;
-            //     console.log({toDelete});
-            //     return rest
-            // }
-            // return {
-            //     ...oldShoppingCart, 
-            //     [product.id]: {...product, count}
-            // }
+            }
+            return {
+                ...oldShoppingCart, 
+                [product.id]: {...product, count}
+            }
             
         })
        
